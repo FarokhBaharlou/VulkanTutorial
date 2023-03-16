562a5fa20f5a0318f032e1cdbbc36b7dfd75a029
Farokh <farokhbahalou@yahoo.com>
2023-03-16 02:19:07 -0400

    updated to use 3d points with homogeneous coordinates

--- VulkanTutorial/src/RenderSystem.cpp
+++ VulkanTutorial/src/RenderSystem.cpp
@@ -10,8 +10,7 @@ namespace MyEngine
 {
 	struct PushConstantData
 	{
-		glm::mat2 transform{ 1.f };
-		glm::vec2 offset;
+		glm::mat4 transform{ 1.f };
 		alignas(16) glm::vec3 color;
 	};
 	RenderSystem::RenderSystem(Device& device, VkRenderPass renderPass) : device{device}
@@ -56,12 +55,12 @@ namespace MyEngine
 		pipeline->bind(commandBuffer);
 		for (auto& obj : gameObjects)
 		{
-			obj.transform2d.rotation = glm::mod(obj.transform2d.rotation + 0.0001f, glm::two_pi<float>());
+			obj.transform.rotation.y = glm::mod(obj.transform.rotation.y + 0.0001f, glm::two_pi<float>());
+			obj.transform.rotation.x = glm::mod(obj.transform.rotation.x + 0.0001f, glm::two_pi<float>());
 
 			PushConstantData push{};
-			push.offset = obj.transform2d.translation;
 			push.color = obj.color;
-			push.transform = obj.transform2d.mat2();
+			push.transform = obj.transform.mat4();
 
 			vkCmdPushConstants(commandBuffer, pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 0, sizeof(PushConstantData), &push);
 			obj.model->bind(commandBuffer);
