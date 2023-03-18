ecc7fac932eb266dfb4b0cfef12fc934fb6db15d
Farokh <farokhbahalou@yahoo.com>
2023-03-18 00:09:19 -0400

    added camera controls

--- VulkanTutorial/src/App.cpp
+++ VulkanTutorial/src/App.cpp
@@ -1,8 +1,10 @@
 #include "App.h"
 #include "RenderSystem.h"
 #include "Camera.h"
+#include "Controller.h"
 #include <stdexcept>
 #include <array>
+#include <chrono>
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
@@ -19,13 +21,19 @@ namespace MyEngine
 	{
 		RenderSystem renderSystem{ device, renderer.getSwapChainRenderPass() };
         Camera camera{};
-        //camera.setViewDirection(glm::vec3(0.f), glm::vec3(0.5f, 0.f, 1.f));
         camera.setViewTarget(glm::vec3(-1., -2.f, 2.f), glm::vec3(0.f, 0.f, 2.5f));
+        auto viewerObject = GameObject::createGameObject();
+        Controller cameraController{};
+        auto currentTime = std::chrono::high_resolution_clock::now();
 		while (!window.shouldClose())
 		{
 			glfwPollEvents();
+            auto newTime = std::chrono::high_resolution_clock::now();
+            float frameTime = std::chrono::duration<float, std::chrono::seconds::period>(newTime - currentTime).count();
+            currentTime = newTime;
+            cameraController.moveInPlaneXZ(window.getGLFWwindow(), frameTime, viewerObject);
+            camera.setViewYXZ(viewerObject.transform.translation, viewerObject.transform.rotation);
             float aspect = renderer.getAspectRatio();
-            //camera.setOrthographicProjection(-aspect, aspect, -1, 1, -1, 1);
             camera.setPerspectiveProjection(glm::radians(50.f), aspect, 0.1f, 10.f);
 			if (auto commandBuffer = renderer.beginFrame())
 			{
