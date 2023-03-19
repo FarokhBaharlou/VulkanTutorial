7c8fe735e8f454ec478d8dd8059ca3e2cbeae422
Farokh <farokhbahalou@yahoo.com>
2023-03-18 22:11:36 -0400

    tiny object loader for loading models

--- VulkanTutorial/src/Model.cpp
+++ VulkanTutorial/src/Model.cpp
@@ -1,5 +1,26 @@
 #include "Model.h"
+#include "Utils.h"
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
 #include <cassert>
+#include <cstring>
+#include <unordered_map>
+
+namespace std
+{
+	template<>
+	struct hash<MyEngine::Model::Vertex>
+	{
+		size_t operator()(MyEngine::Model::Vertex const& vertex) const
+		{
+			size_t seed = 0;
+			MyEngine::hashCombine(seed, vertex.position, vertex.color, vertex.normal, vertex.uv);
+			return seed;
+		}
+	};
+}
 
 namespace MyEngine
 {
@@ -17,6 +38,12 @@ namespace MyEngine
 			vkDestroyBuffer(device.device(), indexBuffer, nullptr);
 			vkFreeMemory(device.device(), indexBufferMemory, nullptr);
 		}
+	}
+	std::unique_ptr<Model> Model::createModelFromFile(Device& device, const std::string& filePath)
+	{
+		Builder builder{};
+		builder.loadModel(filePath);
+		return std::make_unique<Model>(device, builder);
 	}
 	void Model::createVertexBuffers(const std::vector<Vertex>& vertices)
 	{
@@ -96,5 +123,54 @@ namespace MyEngine
 		attributeDescriptions[1].offset = offsetof(Vertex, color);
 
 		return attributeDescriptions;
+	}
+	void Model::Builder::loadModel(const std::string& filePath)
+	{
+		tinyobj::attrib_t attrib;
+		std::vector<tinyobj::shape_t> shapes;
+		std::vector<tinyobj::material_t> materials;
+		std::string warning, error;
+		if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warning, &error, filePath.c_str()))
+		{
+			throw std::runtime_error(warning + error);
+		}
+
+		vertices.clear();
+		indices.clear();
+		std::unordered_map<Vertex, uint32_t> uniqueVertices{};
+		for (const auto& shape : shapes)
+		{
+			for (const auto& index : shape.mesh.indices)
+			{
+				Vertex vertex{};
+				if (index.vertex_index >= 0)
+				{
+					vertex.position = { attrib.vertices[3 * index.vertex_index + 0], attrib.vertices[3 * index.vertex_index + 1] ,attrib.vertices[3 * index.vertex_index + 2] };
+					auto colorIndex = 3 * index.vertex_index + 2;
+					if (colorIndex < attrib.colors.size())
+					{
+						vertex.color = { attrib.colors[colorIndex - 2], attrib.colors[colorIndex - 1] ,attrib.colors[colorIndex - 0] };
+					}
+					else
+					{
+						vertex.color = { 1.f, 1.f, 1.f };
+					}
+				}
+				if (index.normal_index >= 0)
+				{
+					vertex.normal = { attrib.normals[3 * index.normal_index + 0], attrib.normals[3 * index.normal_index + 1] ,attrib.normals[3 * index.normal_index + 2] };
+				}
+				if (index.texcoord_index >= 0)
+				{
+					vertex.uv = { attrib.texcoords[2 * index.texcoord_index + 0], attrib.texcoords[2 * index.texcoord_index + 1] };
+				}
+				if (uniqueVertices.count(vertex) == 0)
+				{
+					uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+					vertices.push_back(vertex);
+				}
+				indices.push_back(uniqueVertices[vertex]);
+			}
+		}
 	}
 }
--- VulkanTutorial/src/Model.h
+++ VulkanTutorial/src/Model.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Device.h"
+#include <memory>
 #include <vector>
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
@@ -13,21 +14,29 @@ namespace MyEngine
 	public:
 		struct Vertex
 		{
-			glm::vec3 position;
-			glm::vec3 color;
+			glm::vec3 position{};
+			glm::vec3 color{};
+			glm::vec3 normal{};
+			glm::vec2 uv{};
 			static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
 			static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+			bool operator==(const Vertex& rhs) const
+			{
+				return position == rhs.position && color == rhs.color && normal == rhs.normal && uv == rhs.uv;
+			}
 		};
 		struct Builder
 		{
 			std::vector<Vertex> vertices{};
 			std::vector<uint32_t> indices{};
+			void loadModel(const std::string& filePath);
 		};
 	public:
 		Model(Device& device, const Model::Builder& builder);
 		~Model();
 		Model(const Model&) = delete;
 		Model& operator=(const Model&) = delete;
+		static std::unique_ptr<Model> createModelFromFile(Device& device, const std::string& filePath);
 		void bind(VkCommandBuffer commandBuffer);
 		void draw(VkCommandBuffer commandBuffer);
 	private:
